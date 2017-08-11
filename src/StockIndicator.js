dea31a5c18e3e494f8d116f12031592db44deecf
meganii <yuhei24@gmail.com>
2017-08-11 23:49:15 +0900

    Fix number of digits

--- src/StockIndicator.js
+++ src/StockIndicator.js
@@ -6,18 +6,21 @@ import './StockIndicator.css'
 
 class StockIndicator extends Component {
   render() {
+    const currentPrice = Number(this.props.currentPrice).toFixed(0);
+    const profitAndLoss = Number(this.props.currentPrice - this.props.previousPrice).toFixed(0);
+
     if ((this.props.currentPrice - this.props.previousPrice) > 0) {
         return (
         <div className="StockIndicator">
             <TrendingUp color={red500} />
-            <div>{this.props.currentPrice}(+{this.props.currentPrice - this.props.previousPrice})</div>
+            <div>{currentPrice}(+{profitAndLoss})</div>
         </div>
         );        
     } else {
         return (
         <div className="StockIndicator">
             <TrendingDown color={greenA200} />
-            <div>{this.props.currentPrice}({this.props.currentPrice - this.props.previousPrice})</div>
+            <div>{currentPrice}({profitAndLoss})</div>
         </div>
         );
     }
