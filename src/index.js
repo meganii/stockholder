98f1b0565196ac5393506ea1760a1be867af6c16
meganii <yuhei24@gmail.com>
2017-08-11 22:44:53 +0900

    Change base color

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import {orange900} from 'material-ui/styles/colors';
 
 import './firebase/config';
 
@@ -14,8 +16,14 @@ import './firebase/config';
 //https://github.com/zilverline/react-tap-event-plugin
 injectTapEventPlugin();
 
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: orange900,
+  },
+});
+
 ReactDOM.render(
-    <MuiThemeProvider>
+    <MuiThemeProvider muiTheme={muiTheme}>
         <App />
     </MuiThemeProvider>,
     document.getElementById('root'));
